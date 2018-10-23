5309cfedcc5d53044b87e41d8b4ea3892de50a55
root <root@Codigito.codigito>
2018-10-23 08:08:44 -0500

    c

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Geolocation } from '@ionic-native/geolocation';
+
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,35 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+	latitud:any;
+	longitud:any;
+
+  constructor(private geolocation: Geolocation,public navCtrl: NavController) {
+
+  	   this.geolocation.getCurrentPosition().then((resp) => {
+ // resp.coords.latitude
+ // resp.coords.longitude
+}).catch((error) => {
+  console.log('Error getting location', error);
+});
+
+let watch = this.geolocation.watchPosition();
+watch.subscribe((data) => {
+ // data can be a set of coordinates, or an error (if an error occurred).
+ // data.coords.latitude
+ // data.coords.longitude
+ alert(data.coords)
+ this.latitud= data.coords.latitude
+ this.longitud = data.coords.longitude
+
+});
+
+
 
   }
+
+
+
+
 
 }
